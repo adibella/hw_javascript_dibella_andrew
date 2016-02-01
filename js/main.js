d1f16f99c23979c921bad0d8511966abd57f362e
Andrew-DiBella <adibella@ufl.edu>
2016-01-31 17:07:14 -0800

    added branch presidents with javascript

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 var iceCream = ["chocolate", "cookiesandcream", "sherbert"];
 iceCream.push("mint");
+
+var presidents = [
+	{firstName: 'Barrack', lastName: 'Obama', termLength: '2', party: 'Democrat', yearsOfPresidency: '8'},
+	{firstName: 'George', lastName: 'Bush', termLength: '2', party: 'Republican', yearsOfPresidency: '8'},
+	{firstName: 'Bill', lastName: 'Clinton', termLength: '2', party: 'Democrat', yearsOfPresidency: '8'},
+	{firstName: 'George H', lastName: 'Bush', termLength: '1', party: 'Republican', yearsOfPresidency: '4'},
+ 	{firstName: 'Ronald', lastName: 'Reagan', termLength: '2', party: 'Republican', yearsOfPresidency: '8'},
+	];
+
+console.log(presidents.firstName[3].lastName[3]);
