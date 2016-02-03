a8483f68af2e2b0e68aa89fc98789b821e97bcee
Andrew-DiBella <adibella@ufl.edu>
2016-02-02 20:31:13 -0800

    reinstalled atom plugins, modified .editorconfig

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
-var iceCream = ["chocolate", "cookiesandcream", "sherbert"];
-iceCream.push("mint");
+var iceCream = [ 'chocolate', 'cookiesandcream', 'sherbert' ];
+iceCream.push( 'mint' );
 
 var presidents = [
-	{firstName: 'Barrack', lastName: 'Obama', termLength: '2', party: 'Democrat', yearsOfPresidency: '8'},
-	{firstName: 'George', lastName: 'Bush', termLength: '2', party: 'Republican', yearsOfPresidency: '8'},
-	{firstName: 'Bill', lastName: 'Clinton', termLength: '2', party: 'Democrat', yearsOfPresidency: '8'},
-	{firstName: 'George H', lastName: 'Bush', termLength: '1', party: 'Republican', yearsOfPresidency: '4'},
- 	{firstName: 'Ronald', lastName: 'Reagan', termLength: '2', party: 'Republican', yearsOfPresidency: '8'},
-	];
+	{ firstName: 'Barrack', lastName: 'Obama', termLength: '2', party: 'Democrat', yearsOfPresidency: '8' },
+	{ firstName: 'George', lastName: 'Bush', termLength: '2', party: 'Republican', yearsOfPresidency: '8' },
+	{ firstName: 'Bill', lastName: 'Clinton', termLength: '2', party: 'Democrat', yearsOfPresidency: '8' },
+	{ firstName: 'George H', lastName: 'Bush', termLength: '1', party: 'Republican', yearsOfPresidency: '4' },
+	{ firstName: 'Ronald', lastName: 'Reagan', termLength: '2', party: 'Republican', yearsOfPresidency: '8' }
+];
 
-console.log(presidents.firstName[3].lastName[3]);
+console.log( presidents.firstName[ 2 ].lastName[ 2 ] );
+
+function howdy() {
+	return 'Andy DiBella says hello!';
+}
+
+console.log( howdy() );
